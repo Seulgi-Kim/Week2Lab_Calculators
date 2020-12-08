5891b2f8030cabf3f07171755a18c511f6c5127b
813116 <813116@SAIT224423IT.ACDM.DS.SAIT.CA>
2020-12-08 11:16:57 -0700

    Add getAll on the UserService

---
+++ src/java/servlets/ArithmeticCalculatorServlet.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author 813116
+ */
+public class ArithmeticCalculatorServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                .forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String first = request.getParameter("first");
+        String second = request.getParameter("second");
+        String plus = request.getParameter("plus");
+        String minus = request.getParameter("minus");
+        String multiply = request.getParameter("multiply");
+        String modulo = request.getParameter("modulo");
+        
+        request.setAttribute("first", first);
+        request.setAttribute("second", second);
+        
+        
+            if (first == null || first.equals("") || second == null || second.equals("")) {
+                request.setAttribute("wrong", "invalid");
+            }
+                else if (plus.equals("+") ) {
+                     request.setAttribute("plus", Integer.parseInt(first) + Integer.parseInt(second));
+                }
+            
+                else if (minus.equals("-") ) {
+                     request.setAttribute("minus", Integer.parseInt(first) - Integer.parseInt(second));
+                }
+            
+                else if (multiply.equals("*") ) {
+                     request.setAttribute("multiply", Integer.parseInt(first) * Integer.parseInt(second));
+                }
+            
+                else {
+                     request.setAttribute("modulo", Integer.parseInt(first) % Integer.parseInt(second));
+                }
+                
+                        
+                    getServletContext().getRequestDispatcher("/WEB-INF/arithmeticcalculator.jsp")
+                .forward(request, response);
+                    return;
+                
+              
+                }
+                
+            }
+        
+        
+    
+
+  
+
+
+
---
+++ web/WEB-INF/arithmeticcalculator.jsp
@@ -0,0 +1,30 @@
+<%-- 
+    Document   : arithmeticcalculator
+    Created on : Dec 8, 2020, 10:19:11 AM
+    Author     : 813116
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Arithmetic Calculator</h1>
+        <form method ="post" action="arithmetic">
+            <label>First:</label><input type="text" name="first" value="${first}"><br>
+            <label>Second:</label><input type="text" name="second" value="${second}"><br>
+                <input type="submit"  name="plus" value="+">
+                <input type="submit"  name="minus" value="-">
+                <input type="submit"  name="multiply" value="*">
+                <input type="submit"  name="modulo" value="%">
+            <br>
+            
+            <p>Result:  ${plus} ${minus} ${multiply} ${modulo} ${wrong}</p>
+            
+            <a href="age">Age Calculator</a>
+        </form>
+    </body>
+</html>
